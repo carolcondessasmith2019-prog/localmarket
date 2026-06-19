01daa3814c43f8f62e02d2cdf6ece10fbdd09550
carolcondessa <carolcondessasmith2019@gmail.com>
2026-06-19 10:52:37 -0300

    Add files via upload

--- src/login/style.js
+++ src/login/style.js
@@ -2,5 +2,34 @@ function login(){
     var nome = $("#nome").val()
     var senha = $("#senha").val()
 
-    if
+    if(nome && senha & nome == "admin" && senha == "12345"){
+        const user = {
+            name: none,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random()* 100000)
+        }
+        localStorage.setItem("usario",JSON.stringify(user))
+        window.location.href="../loja"
+
+    }else{
+        document.getElementById("error-modal").style.display = "flex"
+        document.getElementById("nome").style.borderBottom = "3px solid rgba(239, 108, 128, 0.832)"
+        document.getElementById("senha").style.borderBottom = "3px solid rgba(239, 108, 128, 0.832)"
+    }
+}
+    function showPassword(){
+        var inputSenha = document.querySelector('#senha')
+        var img_eye = document.querySelector('#eye')
+
+        if(inputSenha.getAttribute("type") === "password"){
+           inputSenha.setAttribute("type", "text")
+           img_eye.setAttribute("src", "../../public/view.png")
+        }
+}
+
+
+function fechaError(){
+     document.getElementById("error-modal").style.display = "nome"
+     document.getElementById("nome").style.borderBottom = "2px solid black"
+     document.getElementById("senha").style.borderBottom = "2px solid black"
 }
