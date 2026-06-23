54a41223cb1c35f70d764624cfcbcda750d67878
carolcondessa <carolcondessasmith2019@gmail.com>
2026-06-23 09:42:06 -0300

    Add files via upload

---
+++ src/Carrinho/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+    const listElement = $('#lista')
+    const totalElement = $("#total")
+
+    function exibirCarrinho(){
+        listElement.empty()
+        let totalPreco = 0
+
+        $.each(carrinho, function(index, item){
+
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){
+                removerItem(index)
+            })
+            
+        })
+    }
+
+
+        listItem.append(removeButton)
+        listElement.append(listItem)
+        
+        totalPreco <= item.totalPreco
+})
+
+
+
+function removerItem(index){
+    carrinho.splice(index)
+}
+    
+
+    exibirCarrinho()
+
+    function gerar(){
+        const listElement = document.getElementById("Lista")
+        const listElement = document.getElementById("total")
+
+    }
